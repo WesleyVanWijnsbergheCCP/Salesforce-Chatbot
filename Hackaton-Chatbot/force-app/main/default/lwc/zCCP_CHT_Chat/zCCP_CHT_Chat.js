3e522db85970ff5249554f15f61877a427b53198
Wesley Van Wijnsberghe <wesley.vanwijnsberghe@c-clearpartners.com>
2022-12-15 09:57:14 +0100

    'init'

---
+++ Hackaton-Chatbot/force-app/main/default/lwc/zCCP_CHT_Chat/zCCP_CHT_Chat.js
@@ -0,0 +1,91 @@
+import { LightningElement, track} from 'lwc';
+
+/**
+ * Import Apex Methods
+ */
+import initChat from '@salesforce/apex/zCCP_CHT_Controller.initChat';
+import processChat from '@salesforce/apex/zCCP_CHT_Controller.processChatMessage';
+
+export default class ZCCP_CHT_Chat extends LightningElement {
+
+    /**
+     * Frontend Variables
+     */
+    userName;
+    today;
+    userMessage = null;
+    @track conversation = [];
+
+    /**
+     * Backend Variable - Statefull 
+     */
+    context;
+
+    /**
+     * Error Variable
+     */
+    error; 
+
+    get disableButton(){
+        return this.userMessage ? false : true;
+    }
+
+    connectedCallback(){
+        initChat()
+            .then(context => {
+                this.context = context;
+                this.userName = context.userName;
+                this.setResponse(context.response);
+            })
+            .catch(error => {
+                this.error = error;
+                console.error(error);
+            });
+    }
+
+    setMessage(event){
+        this.userMessage = event.detail.value;
+    }
+
+    submitMessage(event){
+        this.setRequest(this.userMessage);
+        processChat({contextString : JSON.stringify(this.context), message : this.userMessage})
+            .then(context => {
+                this.context = context;
+                this.setResponse(context.response);
+            })
+            .catch(error => {
+                this.error = error;
+                console.error(error);
+            })
+            .finally(() => {
+                this.userMessage = null;
+            })
+    }
+
+    /**
+     *  User Input
+     */
+    setRequest(request){
+        this.today = new Date();
+        let req = {
+            message : request, 
+            isInbound : false,
+            timestamp : this.userName + " • " + String(this.today.getHours()).padStart(2, '0') + ":" + String(this.today.getMinutes()).padStart(2, '0')
+        }
+        this.conversation.push(req);
+    }
+
+    /**
+     * Bot Response
+     */
+    setResponse(response){
+        this.today = new Date();
+        let resp = {
+            message : response,
+            isInbound : true, 
+            timestamp : "AskIndra • " + String(this.today.getHours()).padStart(2, '0') + ":" + String(this.today.getMinutes()).padStart(2, '0')
+        }
+        this.conversation.push(resp);
+    }
+}
